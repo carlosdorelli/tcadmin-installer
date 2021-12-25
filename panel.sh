b5a104ecdf56d976d290589b6e253ee1e7c67167
Carlos Dorelli <carlos@timberhost.com.br>
2021-12-25 15:42:54 -0300

    Rebuilding the script

--- panel.sh
+++ panel.sh
@@ -2,24 +2,120 @@
 
 set -e
 
-# Installing dependencies
-echo -e ""
-echo -e ""
-echo -e "TCAdmin Auto Installer"
-echo -e "https://eldremor.com"
-echo -e ""
-echo -e ""
-echo -e "1. Installing the dependencies of the panel"
-echo -e ""
-apt update;
-apt upgrade;
-apt install gnupg ca-certificates;
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF;
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list;
-apt update;
-apt-get install mono-complete mono-vbnc mono-xsp;
-apt-get install lib32stdc++6;
-apt-get install libpcap0.8 util-linux lsof screen iptables-persistent;
-apt-get install libpcap0.8 util-linux lsof;
-apt-get install libncurses5 libncurses5:i386;
-apt install default-jre;
+######## CHECAGENS GERAIS #########
+
+# SAI COM ERRO SE O USUÁRIO NÃO FOR ROOT
+if [[ $EUID -ne 0 ]]; then
+  echo "* ESTE SCRIPT DEVE SER EXECUTADO POR USUÁRIOS COM PERMISSÃO ROOT (SUDO)" 1>&2
+  exit 1
+fi
+
+########## VARIÁVEIS ############
+GITHUB_SOURCE="master"
+
+## FQDN=""
+
+####### CHECAGEM DE SISTEMA OPERACIONAL #######
+
+detect_distro() {
+  if [ -f /etc/os-release ]; then
+    # FREEDESKTOP.ORG E SYSTEMD
+    . /etc/os-release
+    OS=$(echo "$ID" | awk '{print tolower($0)}')
+    OS_VER=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+    # LINUXBASE.ORG
+    OS=$(lsb_release -si | awk '{print tolower($0)}')
+    OS_VER=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$(echo "$DISTRIB_ID" | awk '{print tolower($0)}')
+    OS_VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS="debian"
+    OS_VER=$(cat /etc/debian_version)
+  elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    OS="SuSE"
+    OS_VER="?"
+  elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    OS="Red Hat/CentOS"
+    OS_VER="?"
+  else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    OS_VER=$(uname -r)
+  fi
+
+  OS=$(echo "$OS" | awk '{print tolower($0)}')
+  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
+}
+
+check_os_comp() {
+  CPU_ARCHITECTURE=$(uname -m)
+  if [ "${CPU_ARCHITECTURE}" != "x86_64" ]; then # check the architecture
+    print_warning "Detected CPU architecture $CPU_ARCHITECTURE"
+    print_warning "Using any other architecture than 64 bit (x86_64) will cause problems."
+
+    echo -e -n "* Are you sure you want to proceed? (y/N):"
+    read -r choice
+
+    if [[ ! "$choice" =~ [Yy] ]]; then
+      print_error "Installation aborted!"
+      exit 1
+    fi
+  fi
+
+  case "$OS" in
+  ubuntu)
+    PHP_SOCKET="/run/php/php8.0-fpm.sock"
+    [ "$OS_VER_MAJOR" == "18" ] && SUPPORTED=true
+    [ "$OS_VER_MAJOR" == "20" ] && SUPPORTED=true
+    ;;
+  debian)
+    PHP_SOCKET="/run/php/php8.0-fpm.sock"
+    [ "$OS_VER_MAJOR" == "9" ] && SUPPORTED=true
+    [ "$OS_VER_MAJOR" == "10" ] && SUPPORTED=true
+    [ "$OS_VER_MAJOR" == "11" ] && SUPPORTED=true
+    ;;
+  centos)
+    PHP_SOCKET="/var/run/php-fpm/pterodactyl.sock"
+    [ "$OS_VER_MAJOR" == "7" ] && SUPPORTED=true
+    [ "$OS_VER_MAJOR" == "8" ] && SUPPORTED=true
+    ;;
+  *)
+    SUPPORTED=false
+    ;;
+  esac
+
+  # exit if not supported
+  if [ "$SUPPORTED" == true ]; then
+    echo "* $OS $OS_VER is supported."
+  else
+    echo "* $OS $OS_VER is not supported"
+    print_error "Unsupported OS"
+    exit 1
+  fi
+}
+
+##### PRINCIPAIS FUNÇÕES DA INSTALAÇÃO #####
+
+# INSTALAR MONO
+install_mono() {
+  echo "* [INSTALAÇÃO] Começando a Instalação do MONO."
+
+
+  
+  curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+  echo "* [INSTALAÇÃO] MONO Instalado com sucesso!"
+}
+
+# Install composer
+install_composer() {
+  echo "* Installing composer.."
+  curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+  echo "* Composer installed!"
+}
--- tcadmin-installer.sh
+++ tcadmin-installer.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-VERSION="v1.0.0"
+SCRIPT_VERSION="v0.1.0"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/carlosdorelli/tcadmin-installer"
 
-# Sai com erro caso o usuário não seja root
+LOG_PATH="/var/log/tcadmin-installer.log"
+
+# SAI COM ERRO SE O USUÁRIO NÃO FOR ROOT
 if [[ $EUID -ne 0 ]]; then
-  echo "* You need root permissions for this script. Do 'sudo su -'." 1>&2
+  echo "* ESTE SCRIPT DEVE SER EXECUTADO POR USUÁRIOS COM PERMISSÃO ROOT (SUDO)" 1>&2
   exit 1
 fi
 
-# Checa pelo curl
+# CHECA PELO CURL
 if ! [ -x "$(command -v curl)" ]; then
-  echo "* O curl é necessário para que o script funcione!"
-  echo "* Instale-o através do comando 'apt install curl'"
+  echo "* O CURL NÃO ESTÁ INSTALADO EM SUA MÁQUINA!"
+  echo "* INSTALE-O PARA QUE O SCRIPT FUNCIONE CORRETAMENTE."
   exit 1
 fi
 
@@ -26,53 +29,60 @@ error() {
   COLOR_NC='\033[0m'
 
   echo ""
-  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo -e "* ${COLOR_RED}[ERRO]${COLOR_NC} $1"
   echo ""
+}
+
+execute() {
+  echo -e "\n\n* tcadmin-installer $(date) \n\n" >> $LOG_PATH
+
+  bash <(curl -s "$1") | tee -a $LOG_PATH
+  [[ -n $2 ]] && execute "$2"
 }
 
 done=false
 
 output
-output "TCAdmin Installer @ $VERSION"
+output "Script de Instalação TCAdmin @ $SCRIPT_VERSION"
 output
-output "Copyright (C) 2021, Carlos 'Eldremor' Dorelli, <carlos@timberhost.com.br>"
-output "https://github.com/eldremor/tcadmin-installer"
+output "Copyright (C) 2021 - 2022, Carlos Dorelli, <itscarlosdorelli@gmail.com>"
+output "https://github.com/carlosdorelli/tcadmin-installer"
 output
-output "This is custom script and it's not related to TCAdmin.com"
-
+output "Para doações: https://github.com/carlosdorelli/tcadmin-installer?sponsor=1"
+output "Este Script não tem ligação oficial com o TCAdmin, é de terceiros."
 output
 
-panel() {
-  bash <(curl -s https://raw.githubusercontent.com/eldremor/tcadmin-installer/panel.sh)
-}
+PAINEL_NORMAL="$GITHUB_BASE_URL/$SCRIPT_VERSION/panel.sh"
 
-mysql() {
-  bash <(curl -s https://raw.githubusercontent.com/eldremor/tcadmin-installer/mysql.sh)
-}
+PANEL_NGINX="$GITHUB_BASE_URL/$SCRIPT_VERSION/panel-nginx.sh"
+
+PANEL_MYSQL_NGINX="$GITHUB_BASE_URL/$SCRIPT_VERSION/panel-nginx-mysql.sh"
 
 while [ "$done" == false ]; do
   options=(
-    "Install TCAdmin"
-    "Install MYSQL + Mariadb"
+    "Instalar o painel"
+    "Instalar o painel com NGINX (SSL)"
+    "Instalar o painel com MYSQL e NGINX (SSL) [RECOMENDADO]"
   )
 
   actions=(
-    "panel"
-    "mysql"
+    "$PANEL"
+    "$PANEL_NGINX"
+    "$PANEL_MYSQL_NGINX;"
   )
 
-  output "Which option do you choose?"
+  output "[INSTALADOR] O que você quer fazer?"
 
   for i in "${!options[@]}"; do
     output "[$i] ${options[$i]}"
   done
 
-  echo -n "* Input 0-$((${#actions[@]}-1)): "
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
   read -r action
 
-  [ -z "$action" ] && error "Choose a option" && continue
+  [ -z "$action" ] && error "Você precisa escolher uma opção!" && continue
 
-  valid_input=("$(for ((i=0;i<=${#actions[@]}-1;i+=1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option!"
-  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && eval "${actions[$action]}"
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Opção Inválida!"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<< "${actions[$action]}" && execute "$i1" "$i2"
 done
